bb452a8dcc18f865aad7d0268caeae6d252cc617
alessandravolfe <alessandravolfe3@gmail.com>
2026-07-13 15:24:11 -0300

    Update admin-login.js

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,1 +1,27 @@
+// ================================
+// LOGIN ADMINISTRADOR - VERBUM
+// ================================
 
+const SENHA_ADMIN = "Verbum@2026";
+
+const formulario = document.getElementById("adminLogin");
+
+formulario.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const senha = document.getElementById("senha").value;
+
+    if(senha === SENHA_ADMIN){
+
+        sessionStorage.setItem("adminLogado","true");
+
+        window.location.href="adm.html";
+
+    }else{
+
+        document.getElementById("erro").style.display="block";
+
+    }
+
+});
